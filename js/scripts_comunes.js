10e3f71a1e7e45d26dde5c4797b63a56580c56b6
Johanna Palo Bael <joa.palobael@gmail.com>
2020-10-11 10:16:52 -0300

    PF terminado

--- js/scripts_comunes.js
+++ js/scripts_comunes.js
@@ -1,8 +1,8 @@
 // BOTON DE IR ARRIBA
 window.onscroll = function(){
     if(document.documentElement.scrollTop > 100){
-        document.querySelector('.go-top-container').classList.add('show');
-    } else { document.querySelector('.go-top-container').classList.remove('show');}
+        document.querySelector('.go-top-container').classList.add('showed');
+    } else { document.querySelector('.go-top-container').classList.remove('showed');}
 }
 document.querySelector('.go-top-container').addEventListener('click', () =>{
     window.scrollTo({
