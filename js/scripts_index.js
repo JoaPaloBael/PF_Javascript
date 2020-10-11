f6b85501f7c976b54a7fee4f38b7e79386321849
Johanna Palo Bael <joa.palobael@gmail.com>
2020-10-10 23:11:35 -0300

    YA casi esta todo listo

--- js/scripts_index.js
+++ js/scripts_index.js
@@ -63,18 +63,3 @@ function iniciarStaff(){
 }
 
 iniciarStaff();
-
-
-// BOTON DE IR ARRIBA
-
-window.onscroll = function(){
-    if(document.documentElement.scrollTop > 100){
-        document.querySelector('.go-top-container').classList.add('show');
-    } else { document.querySelector('.go-top-container').classList.remove('show');}
-}
-document.querySelector('.go-top-container').addEventListener('click', () =>{
-    window.scrollTo({
-        top: 0, 
-        behavior: "smooth"
-    })
-});
